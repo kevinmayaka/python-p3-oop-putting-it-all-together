128750fbe7be5b4e80dccb37525cf301b1ada5d1
kevin mayaka <kevinabuga3@gmail.com>
2024-05-28 11:17:26 +0300

    initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,52 @@
 #!/usr/bin/env python3
 
+import io
+import sys
+
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count  # This uses the setter method
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# Testing the Book class to ensure it passes the provided tests
+def run_tests():
+    # Test case 1: has title and page_count
+    book1 = Book("And Then There Were None", 272)
+    assert(book1.page_count == 272)
+    assert(book1.title == "And Then There Were None")
+
+    # Test case 2: requires int page_count
+    book2 = Book("And Then There Were None", 272)
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    book2.page_count = "not an integer"
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "page_count must be an integer\n"
+
+    # Test case 3: can turn page
+    book3 = Book("The World According to Garp", 69)
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    book3.turn_page()
+    sys.stdout = sys.__stdout__
+    assert(captured_out.getvalue() == "Flipping the page...wow, you read fast!\n")
+
+    print("All tests passed!")
+
+# Running the tests
+run_tests()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,71 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size  # This uses the setter method
+        self.condition = None
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+# Testing the Shoe class to ensure it passes the provided tests
+def run_tests():
+    import io
+    import sys
+
+    class TestShoe:
+        '''Shoe in shoe.py'''
+
+        def test_has_brand_and_size(self):
+            '''has the brand and size passed to __init__, and values can be set to new instance.'''
+            stan_smith = Shoe("Adidas", 9)
+            assert(stan_smith.brand == "Adidas")
+            assert(stan_smith.size == 9)
+
+        def test_requires_int_size(self):
+            '''prints "size must be an integer" if size is not an integer.'''
+            stan_smith = Shoe("Adidas", 9)
+            captured_out = io.StringIO()
+            sys.stdout = captured_out
+            stan_smith.size = "not an integer"
+            sys.stdout = sys.__stdout__
+            assert captured_out.getvalue() == "size must be an integer\n"
+
+        def test_can_cobble(self):
+            '''says that the shoe has been repaired.'''
+            stan_smith = Shoe("Adidas", 9)
+            captured_out = io.StringIO()
+            sys.stdout = captured_out
+            stan_smith.cobble()
+            sys.stdout = sys.__stdout__
+            assert(captured_out.getvalue() == "Your shoe is as good as new!\n")
+        
+        def test_cobble_makes_new(self):
+            '''creates an attribute on the instance called 'condition' and set equal to 'New' after repair.'''
+            stan_smith = Shoe("Adidas", 9)
+            stan_smith.cobble()
+            assert(stan_smith.condition == "New")
+
+    test_shoe = TestShoe()
+    test_shoe.test_has_brand_and_size()
+    test_shoe.test_requires_int_size()
+    test_shoe.test_can_cobble()
+    test_shoe.test_cobble_makes_new()
+    print("All tests passed!")
+
+# Running the tests
+run_tests()
